acf732d17234c8e7f50d176ce076ea65425e1c2a
lullabyeoytl <2199288762@qq.com>
2024-07-19 20:42:55 +0800

    update week4

---
+++ week4/ice-webdev1-main/starter/src/components/Badger.jsx
@@ -0,0 +1,9 @@
+import { Card } from "react-bootstrap";
+
+function Badger(props) {
+    return <Card style={{margin: "0.5rem"}}>
+        <h2>Bucky Badger</h2>
+    </Card>
+}
+
+export default Badger;
---
+++ week4/ice-webdev1-main/starter/src/components/FindMyBadgers.jsx
@@ -0,0 +1,12 @@
+import Badger from "./Badger";
+
+function FindMyBadgers() {
+    return <div>
+        <h1>Find My Badgers</h1>
+        <Badger></Badger>
+        <Badger></Badger>
+        <Badger></Badger>
+    </div>
+}
+
+export default FindMyBadgers;
